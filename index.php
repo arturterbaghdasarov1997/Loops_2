f968cd5c9c9d176d77397cf670f38b1ede981375
unknown <terbagdasarovartur@gmail.com>
2024-02-02 21:40:27 +0400

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+   $lastItaration = 100;
+   $i = 1;
+
+do {
+    echo "$i, ";
+    $i++;
+if ($i < 51) {
+   echo "პირველი ნახევარი";
+} else {
+   echo "მეორე ნახევარი";
+}
+   } while ($i <= 100);
